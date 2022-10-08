2b3ee80109365bef9c1e13d2acabbcd3ddf93322
Michaelexe <michaelispahani@gmail.com>
2022-10-08 17:46:01 -0400

    added more tabs and screens

--- frontend/App.js
+++ frontend/App.js
@@ -5,27 +5,74 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import ClubScreen from './screens/ClubsScreen';
 import SettingsScreen from './screens/SettingsScreen';
+import CreateScreen from './screens/CreateScreen';
+import JoinScreen from './screens/JoinScreen';
 
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 function App() {
   const Tab = createBottomTabNavigator();
   return (
     <NavigationContainer>
       <Tab.Navigator
+        sceneContainerStyle={{backgroundColor: '#111111'}}
         screenOptions={({route}) => ({
           tabBarIcon: ({focused, color, size}) => {
             if (route.name === 'Clubs') {
-              return <FontAwesome name="group" color={color} size={30} />;
+              return (
+                <MaterialCommunityIcons
+                  name="account-group-outline"
+                  color={color}
+                  size={focused ? 31 : 28}
+                />
+              );
             } else if (route.name === 'Setting') {
-              return <Ionicons name="settings" color={color} size={size} />;
+              return (
+                <Ionicons
+                  name="settings-outline"
+                  color={color}
+                  size={focused ? 31 : 28}
+                />
+              );
+            } else if (route.name === 'Join') {
+              return (
+                <Ionicons
+                  name="enter-outline"
+                  color={color}
+                  size={focused ? 31 : 28}
+                />
+              );
+            } else if (route.name === 'Create') {
+              return (
+                <Ionicons
+                  name="create-outline"
+                  color={color}
+                  size={focused ? 31 : 28}
+                />
+              );
             }
           },
-          tabBarActiveTintColor: 'darkblue',
+          tabBarActiveTintColor: '#57e6ff',
           tabBarInactiveTintColor: 'gray',
+          tabBarStyle: {
+            backgroundColor: '#111111',
+            height: 60,
+          },
+          tabBarActiveBackgroundColor: 'black',
+          tabBarLabelStyle: {
+            fontSize: 15,
+            fontWeight: '900',
+          },
+          headerStyle: {
+            backgroundColor: '#111111',
+          },
+          headerTintColor: 'white',
         })}>
         <Tab.Screen name="Clubs" component={ClubScreen} />
+        <Tab.Screen name="Join" component={JoinScreen} />
+        <Tab.Screen name="Create" component={CreateScreen} />
         <Tab.Screen name="Setting" component={SettingsScreen} />
       </Tab.Navigator>
     </NavigationContainer>

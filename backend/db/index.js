ee196959526a62d7b1311d3dc8ac64c886ff22cd
David Houle-Tymeczko <david.houletymeczko@ontariotechu.net>
2022-10-16 16:13:21 -0400

    Minor progress on routes

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,11 +1,12 @@
 const { Pool } = require("pg");
 
-const Pool = new Pool({
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-});
+const db = new Pool();
+//   {
+//   user: process.env.PGUSER,
+//   password: process.env.PGPASSWORD,
+//   port: process.env.PGPORT,
+//   host: process.env.PGHOST,
+//   database: process.env.PGDATABASE,
+//   }
 
-module.exports = { Pool };
+module.exports = { db };
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const auth = require("auth");
+const { db } = require("../db");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+function isUser(email) {
+  // look into select exists
+  db.query("EXISTS (SELECT $1 from member)", email);
+}
+
+/* POST register new member */
+router.post("/register", (req, res, next) => {
+  // Check if user already exists
+  // Add new member
+  // db.query("INSERT INTO member ()");
 });
 
 module.exports = router;
